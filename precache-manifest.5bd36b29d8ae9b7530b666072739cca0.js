549898353fe8f9bdb9526bb7ba04e5b71cc7a1fb
GeorgeWu <fysidm@gmail.com>
2020-05-02 17:45:30 +0800

    Updates

---
+++ precache-manifest.5bd36b29d8ae9b7530b666072739cca0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c001fbd84bbbca4663169beb2a46b66f",
+    "url": "/smartbee-frontend-interview/index.html"
+  },
+  {
+    "revision": "21f029082563c11d41bf",
+    "url": "/smartbee-frontend-interview/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "94124bcc868309b5c9bc",
+    "url": "/smartbee-frontend-interview/static/js/2.5ffc4f95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smartbee-frontend-interview/static/js/2.5ffc4f95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21f029082563c11d41bf",
+    "url": "/smartbee-frontend-interview/static/js/main.d136199a.chunk.js"
+  },
+  {
+    "revision": "1dffa8eedfca748fb794",
+    "url": "/smartbee-frontend-interview/static/js/runtime-main.9e6e7296.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smartbee-frontend-interview/static/media/logo.5d5d9eef.svg"
+  }
+]);
